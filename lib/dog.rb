088f7beb0e5c0339e3489e83cdff81cf575e1d53
simokh <m.simokhouili@gmail.com>
2020-04-28 05:50:46 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,17 @@
 require 'pry'
+class Dog
 
-  class Dog
-    def name=(dog_name)
-      @this_dogs_name= dog_name
-    end
+  attr_accessor :name
 
+  # def name= (dog_name)
+  #   @dog_name = dog_name
+  # end
+  #
+  # def name
+  #   @dog_name
+  # end
 
-    def name
-      @this_dogs_name
-    end
+  def bark
+    puts "woof!"
   end
-
-    class Dog
-    def bark
-      puts "woof!"
-    end
-  end
-
-
-
-
-
-
-
-
-
-
-
-# puts dog.name
+end
